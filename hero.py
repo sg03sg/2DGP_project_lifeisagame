579cbc249b25596b52a15b8ddce1e5b1c192213b
seunggyunJung <csg1221sg@gmail.com>
2025-11-01 23:43:59 +0900

    점프 함수 추가 및 기능 구현  초기상승속도 v 늘리면 높이 감 gravity는 올리면 부자연스러워져서 잘 맞춰야함

--- hero.py
+++ hero.py
@@ -1,16 +1,34 @@
 from pico2d import load_image
-import state_machine
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE
 from state_machine import StateMachine
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def jump_end(e):
+    return e[0] == 'jump_end'
+
+def No(e):
+    return False
+
+def hero_jump(hero, dt):
+    hero.jump_vy += hero.gravity * dt
+    hero.y += hero.jump_vy * dt
+    # 착지 검사
+    if hero.y <= 150:
+        hero.y = 150
+        hero.jump_vy = 0.0
+        hero.state_machine.handle_state_event(("jump_end", None))
 
 
 class Run:
     def __init__(self,hero):
         self.hero = hero
 
-    def enter(self):
+    def enter(self,e):
         pass
 
-    def exit(self):
+    def exit(self,e):
         pass
 
     def do(self):
@@ -26,9 +44,9 @@ class Idle:
     def __init__(self, hero):
         self.hero = hero
 
-    def enter(self):
+    def enter(self,e):
         pass
-    def exit(self):
+    def exit(self,e):
         pass
     def do(self):
         pass
@@ -39,14 +57,22 @@ class Jump:
     def __init__(self, hero):
         self.hero = hero
 
-    def enter(self):
+    def enter(self,e):
+        # 점프 시작 시 초기 속도 설정
+        self.hero.jump_vy = self.hero.jump_initial_v
+
+    def exit(self,e):
         pass
-    def exit(self):
-        pass
+
     def do(self):
-        pass
+        # dt는 lifeisagame.py의 delay 값과 맞춰 사용
+        dt = 0.02
+        hero_jump(self.hero, dt)
+
     def draw(self):
-        pass
+        self.hero.image.clip_composite_draw(self.hero.frame * 153, 323 - self.hero.y_frame * 113, 120, 90, 0, 'h',
+                                            self.hero.x, self.hero.y, 100,
+                                            100)
 
 class Hero:
     def __init__(self):
@@ -55,11 +81,22 @@ class Hero:
         self.y_frame =-1
         self.image = load_image('nobaby.png')
 
+        # 점프 관련 기본값 : v0^2 / (2 * |g|) <-이거 계산하면 최고 높이
+        self.jump_initial_v = 1300.0    # 초기 상승 속도(px/s)
+        self.gravity = -2100.0         # 중력(px/s^2)
+        self.jump_vy = 0.0
+
         self.run = Run(self)
         self.idle = Idle(self)
         self.jump = Jump(self)
         self.state_machine = StateMachine(
-              self.run)
+              self.run,
+        {
+                self.run: {space_down: self.jump},
+                self.jump: {jump_end: self.run},
+                self.idle: {No: self.idle},
+             }
+        )
 
 
     def update(self):
@@ -71,5 +108,4 @@ class Hero:
         pass
 
     def handle_event(self, event):
-        pass
-        # self.state_machine.handle_state_event(("INPUT", event))
+        self.state_machine.handle_state_event(("INPUT", event))
--- state_machine.py
+++ state_machine.py
@@ -1,19 +1,24 @@
-
 class StateMachine:
-    def __init__(self, start_state):
+    def __init__(self, start_state, rules):
         self.cur_state = start_state
-        # self.rules = rules
-        # self.cur_state.enter(('START', None))
+        self.rules = rules
+        self.cur_state.enter(("START", None))
+
     def update(self):
         self.cur_state.do()
 
     def draw(self):
         self.cur_state.draw()
 
-    # def handle_state_event(self, state_event):
-    #     for check_event in self.rules[self.cur_state].keys():
-    #         if check_event(state_event):
-    #             next_state = self.rules[self.cur_state][check_event]
-    #             self.cur_state.exit(state_event)
-    #             next_state.enter(state_event)
+    def handle_state_event(self, state_event):
+         # 현재 상태에 대한 전이 규칙이 없으면 아무 것도 하지 않음
+        # if self.cur_state not in self.rules:
+        #     return
 
+        for check_event in self.rules[self.cur_state].keys():
+            if check_event(state_event):
+                next_state = self.rules[self.cur_state][check_event]
+                self.cur_state.exit(state_event)
+                next_state.enter(state_event)
+                self.cur_state = next_state
+            return
