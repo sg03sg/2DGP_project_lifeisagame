6d63963bc6ab99704eb3a811d514ebdccd4190e0
seunggyunJung <csg1221sg@gmail.com>
2025-11-10 00:33:37 +0900

    main.py play_mode.py로 모듈 분리

--- lifeisagame.py
+++ lifeisagame.py
@@ -1,57 +1,13 @@
 from pico2d import *
-
-from background import Background
-from hero import Hero
-
-def handle_events():
-    global running
-
-    event_list = get_events()
-    for event in event_list:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        else:
-            hero.handle_event(event)
-
-
-def reset_world():
-    global world
-    global hero
-
-    world = []
-
-    background = Background()
-    world.append(background)
-
-    hero = Hero()
-    world.append(hero)
-
-
-
-def update_world():
-    for o in world:
-        o.update()
-    pass
-
-
-def render_world():
-    clear_canvas()
-    for o in world:
-        o.draw()
-    update_canvas()
-
-
-running = True
+import play_mode
 
 open_canvas(1280,720)
-reset_world()
+play_mode.init()
 # game loop
-while running:
-    handle_events()
-    update_world()
-    render_world()
+while play_mode.running:
+    play_mode.handle_events()
+    play_mode.update_world()
+    play_mode.draw()
     delay(0.08)
-# finalization code
+play_mode.finish()
 close_canvas()
