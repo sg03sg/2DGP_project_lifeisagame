b5ffe8e1ffa22cc8cdb8f8fb89a5ac6ec6af1310
seunggyunJung <csg1221sg@gmail.com>
2025-11-12 04:09:41 +0900

    젖병 아이템 생성 및 왼쪽으로 움직이게 하는 클래스 추가

---
+++ item.py
@@ -0,0 +1,38 @@
+from pico2d import *
+import game_world
+import game_framework
+import background
+
+class Item:
+    image = None
+
+    def __init__(self, y):
+        if Item.image == None:
+            Item.image = load_image('ITEMIMAGE_babymilk.png')
+        self.x = 1300
+        self.y = y
+        self.speed = background.RUN_SPEED_PPS + 500  # 아이템의 속도 (배경보다 빠르게)
+        self.xv = -self.speed  # x 축 속도
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+
+    def draw(self):
+        self.image.draw(self.x, self.y,40,40)
+        draw_rectangle(*self.get_bb())
+
+    def handle_collision(self,group, other):
+        pass
+        # if group == 'boy:Item':
+        #     game_world.remove_object(self)
+        # elif group == 'grass:Item':
+        #     self.stopped = True
+        #     game_world.remove_collision_object(self)
+        # elif group == 'zombie:Item':
+        #     game_world.remove_object(self)
+
+    def update(self):
+        # 위치 업데이트
+        self.x += self.xv * game_framework.frame_time
+        if self.x <= 0:
+            game_world.remove_object(self)
