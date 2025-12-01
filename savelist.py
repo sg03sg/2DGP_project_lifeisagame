8e3dd3194ed259db6ee9037e3019a526b40a50c0
seunggyunJung <csg1221sg@gmail.com>
2025-12-02 04:31:44 +0900

    y축 마다 타이머 간격 저장

--- savelist.py
+++ savelist.py
@@ -3,7 +3,7 @@ class Itemlist:
     def __init__(self):
         self.item_pos = [[150 ,350], [570],[550,450,350,250,150]]
         self.max_item_count = [[inf], [inf] , [9,9,9,9] ]
-        self.y_timer_interval = [[1.5,3.0], [1.5], [2.0,3.0,3.5,4.0,4.5]]
+        self.y_timer_interval = [[1.5,3.0], [1.2], [2.0,3.0,3.5,4.0,5.0]]
 
 class Uilist:
     def __init__(self):
