108bb3b980fb06a46d0453946273b9cf1c5c5e9c
seunggyunJung <csg1221sg@gmail.com>
2025-12-03 01:42:47 +0900

    age2 아이템 ui 정보를 담은 배열 추가

--- savelist.py
+++ savelist.py
@@ -1,13 +1,11 @@
 inf = float('inf')
-class Itemlist:
-    def __init__(self):
-        self.item_pos = [[150 ,350], [570],[600,500,400,300,150]]
-        self.max_item_count = [[inf], [inf] , [12,11,11,11] ]
-        self.y_timer_interval = [[2.0,3.5], [1.5], [4.0,5.0,5.5,6.5,7.0]]
+item_pos = [[150 ,350], [570],[600,500,400,300,150]]
+max_item_count = [[inf], [inf] , [12,11,11,11] ]
+y_timer_interval = [[2.0,3.5], [1.5], [4.0,5.0,5.5,6.5,7.0]]
+skillname = ['hobby', 'friend', 'family']
 
-class Uilist:
-    def __init__(self):
-        self.skillname = ['hobby', 'friend', 'family']
-
-        self.age1uiname = ['smart', 'baby', 'painting']
-        self.age1ui_max_count = [9, 3, 3]
+## -------------------------------------------
+age1uiname = ['smart', 'baby', 'painting']
+age2uiname = ['study', 'paint', 'music', 'soccer']
+age1ui_max_count = [9, 3, 3]
+age2ui_max_count = [9, 9, 9, 9]
