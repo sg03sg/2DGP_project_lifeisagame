1c9728f71972ca8a2c86119d64cbedec5736a4b1
seunggyunJung <csg1221sg@gmail.com>
2025-12-10 05:38:48 +0900

    여러 스킬이 사용하도록 마지막 사용시간을 배열로 저장

--- skill_system.py
+++ skill_system.py
@@ -6,7 +6,7 @@ class Skill_system():
     def __init__(self):
         self.skills = common.skills
         self.cooltime = 6.0
-        self.last_use_time = 0
+        self.last_use_time = [0 for _ in range(len(self.skills))]
         self.can_skill_use = [True for _ in range(len(self.skills))]
 
     ##스킬 쿨타임 계산 쿨타임 일때는 스킬 쿨타임 상태 ui로 바꾸고 아닐때는 스킬 ui 활성화 상태
@@ -14,7 +14,7 @@ class Skill_system():
         if self.skills[skill_num].skill_earn == False:
             return
         current_time = time.time()
-        if current_time - self.last_use_time >= self.cooltime:
+        if current_time - self.last_use_time[skill_num] >= self.cooltime:
            self.skills[skill_num].skill_on = True
 
         else:
@@ -25,7 +25,7 @@ class Skill_system():
             return
         if self.skills[skill_num].skill_on:
             common.effect.skill_effect_play(skill_num)
-            self.last_use_time = time.time()
+            self.last_use_time[skill_num] = time.time()
             self.skills[skill_num].on = False
             common.hero.happy = min(100, common.hero.happy + 10)
     def update(self):
