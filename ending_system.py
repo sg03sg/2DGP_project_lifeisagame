d5dbb286bf18760ebdb873f2ea9a14053aa61379
seunggyunJung <csg1221sg@gmail.com>
2025-12-10 01:24:51 +0900

    죽었을때 3초 뒤에 커튼 이미지 출력

--- ending_system.py
+++ ending_system.py
@@ -3,14 +3,18 @@ import common
 import game_framework
 import game_world
 import savelist
-from background import BOTTOM_OFFSET
 from hero import Old_die
+import ending_mode
+
+with open('Json/curtain_data.json', 'r', encoding='utf-8') as f:
+    cutain = json.load(f)
+curtain_data = cutain['sprites']
 
 class Ending_system:
     def __init__(self):
         self.ending_image = None
         self.perfect = False
-        self.smokeen = False
+        self.smokken = False
         self.sucide = False
 
         self.ending = False
@@ -18,8 +22,9 @@ class Ending_system:
 
     def ending_judge(self):
         if self.ending:
-            if get_time() - self.time >= 4.0:
-                game_framework.change_mode(ending_mode)
+            if get_time() - self.time >= 3.0:
+                curtain = Curtain()
+                game_world.add_object(curtain,3)
 
         else:
             if self.perfect:
@@ -48,14 +53,15 @@ class Ending_system:
             self.smokken = True
         elif hero.happy <= 0:
             self.sucide = True
-        elif bg.map_idx == 15 and bg.hero_pos == bg.total_w[19]:
+        elif bg.map_idx == 15 and bg.hero_pos >= bg.total_w[19]//2:
             self.perfect = True
 
-    def draw(self):
-        pass
+        self.ending_judge()
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+BOTTOM_OFFSET = 100
+
 
 class Game_over():
     def __init__(self,num):
@@ -71,3 +77,35 @@ class Game_over():
 
     def draw(self):
         self.images[self.num].draw(self.x,self.y,self.w,self.h)
+
+TIME_PER_ACTION = 2.0 #커튼 닫힐때 2초
+ACTION_PER_TIME = 1.0 /TIME_PER_ACTION
+FRAMES_PER_ACTION = 11
+
+class Curtain():
+    def __init__(self):
+        filename = ["Images/curtain1.png","Images/curtain2.png","Images/curtain3.png"]
+        self.images = [load_image(f) for f in filename]
+        self.time = get_time()
+        self.frame_move = True
+        self.frame = 0
+
+    def update(self):
+        if get_time() - self.time >= 3.0:
+            print('end')
+        if not self.frame_move:
+            return
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 11
+        if int(self.frame) >= 9:
+            frame_move = False
+
+    def draw(self):
+        start_index = [4,7,11]
+        i = int(self.frame)
+        img_i = 0
+        for idx,val in enumerate(start_index):
+            if i < val:
+                img_i = idx
+                break
+        frame_data = curtain_data[i]
+        self.images[img_i].clip_draw(int(frame_data["x"]),int(frame_data["y"]),int(frame_data["width"]),int(frame_data["height"]),SCREEN_WIDTH//2,SCREEN_HEIGHT//2,1280,720)
